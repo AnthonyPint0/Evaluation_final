24b7b350f2ae951a1d75833ed834b5e77004bcec
DarkPrinceLD <antoniocruze17@gmail.com>
2024-05-27 21:28:24 +0530

    changes in add.c

--- add.c
+++ add.c
@@ -4,15 +4,15 @@ int main() {
     int num1, num2, sam;
     char a;
     
-    printf("Enter the first number: ");
+    printf("Enter the 1st number: ");
     scanf("%d", &num1);
     
-    printf("Enter the second number: ");
+    printf("Enter the 2nd number: ");
     scanf("%d", &num2);
     
     sam = num1 + num2;
     
-    printf("Thesam of %d and %d is %d.", num1, num8, sam);
+    printf("The sum of %d and %d is %d.", num1, num8, sam);
     
     return 0;
 }
